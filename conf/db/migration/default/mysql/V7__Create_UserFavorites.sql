83aa594a985a006a69cd2808e82b07e56b5b7b9e
tofu511 <mokmok511@gmail.com>
2018-01-03 21:16:25 +0900

    fix typo

--- conf/db/migration/default/mysql/V7__Create_UserFavorites.sql
+++ conf/db/migration/default/mysql/V7__Create_UserFavorites.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `favorites` (
     `id`            BIGINT          AUTO_INCREMENT,
     `user_id`       BIGINT          NOT NULL,
-    `micropost_id`  BIGINT          NOT NULL,
+    `microPost_id`  BIGINT          NOT NULL,
     `create_at`     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_at`     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES users(`id`),
-    FOREIGN KEY (`micropost_id`) REFERENCES micro_posts(`id`),
-    UNIQUE(`user_id`, `micropost_id`)
+    FOREIGN KEY (`microPost_id`) REFERENCES micro_posts(`id`),
+    UNIQUE(`user_id`, `microPost_id`)
 ) ENGINE=InnoDB;
--- conf/db/migration/default/postgresql/V7__Create_UserFavorites.sql
+++ conf/db/migration/default/postgresql/V7__Create_UserFavorites.sql
@@ -1,11 +1,11 @@
 CREATE TABLE favorites (
     id            BIGSERIAL       NOT NULL,
     user_id       BIGINT          NOT NULL,
-    favorite_id   BIGINT          NOT NULL,
+    microPost_id  BIGINT          NOT NULL,
     create_at     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_at     TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (favorite_id) REFERENCES micro_posts(id),
-    UNIQUE(user_id, favorite_id)
+    FOREIGN KEY (microPost_id) REFERENCES micro_posts(id),
+    UNIQUE(user_id, microPost_id)
 );
