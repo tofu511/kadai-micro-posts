3e81f63e5f4c3caead8a36a5688cbdf05749af74
tofu511 <mokmok511@gmail.com>
2018-01-03 20:49:07 +0900

    add migration files

---
+++ conf/db/migration/default/mysql/V8__Add_Favorites.sql
@@ -0,0 +1,8 @@
+INSERT INTO `favorites` (`user_id`, `micropost_id`, `create_at`, `update_at`) VALUES (1, 3, now(), now());
+INSERT INTO `favorites` (`user_id`, `micropost_id`, `create_at`, `update_at`) VALUES (1, 4, now(), now());
+INSERT INTO `favorites` (`user_id`, `micropost_id`, `create_at`, `update_at`) VALUES (1, 5, now(), now());
+INSERT INTO `favorites` (`user_id`, `micropost_id`, `create_at`, `update_at`) VALUES (1, 6, now(), now());
+INSERT INTO `favorites` (`user_id`, `micropost_id`, `create_at`, `update_at`) VALUES (2, 5, now(), now());
+INSERT INTO `favorites` (`user_id`, `micropost_id`, `create_at`, `update_at`) VALUES (2, 6, now(), now());
+INSERT INTO `favorites` (`user_id`, `micropost_id`, `create_at`, `update_at`) VALUES (2, 7, now(), now());
+INSERT INTO `favorites` (`user_id`, `micropost_id`, `create_at`, `update_at`) VALUES (2, 8, now(), now());
